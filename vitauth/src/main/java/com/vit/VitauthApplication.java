868b411bc07c60d8f5c1b05b0ac24a8cd8f8e1d4
zoucailue <zoucailue@windows10.microdone.cn>
2019-06-25 00:16:15 +0800

    starting vitauth

---
+++ vitauth/src/main/java/com/vit/VitauthApplication.java
@@ -0,0 +1,13 @@
+package com.vit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitauthApplication.class, args);
+	}
+
+}
